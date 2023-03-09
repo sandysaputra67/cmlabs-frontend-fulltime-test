1d7561f0616ef8d4b1ad2bd934dfea6bbffb36a1
sandy saputra <ss5679848@gmail.com>
2023-03-09 11:20:05 +0700

    first commit

---
+++ src/components/MealItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="bg-light shadow rounded-xl hover:scale-105 transition-all "
+  >
+    <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
+      <img
+        :src="meal.strMealThumb"
+        :alt="meal.strMeal"
+        class="rounded-t-xl w-full h-48 object-cover"
+        
+      >
+      <h3 class="font-bold">{{ meal.strMeal }}</h3>
+      
+    </router-link>
+  </div>
+
+</template>
+
+<script setup>
+
+const { meal } = defineProps({
+  meal: {
+    required: true,
+    type: Object
+  }
+})
+
+</script>
